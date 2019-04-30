628d71f68ebb72a14d8b72534eb48ea5b4fe629f
Alex Bik <alex@tarthorst.net>
2019-04-30 08:25:24 +0200

    Combined TemperatureSensors.ino and tanklevel.ino into Sensors.ino

--- Sensors.ino
+++ Sensors.ino
@@ -1,3 +1,13 @@
+void readTemperatureSensors() {
+  DallasTemperature sensors(&oneWire);
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  for(int sensorNr=0;sensorNr<10;sensorNr++) {
+    float temperature = sensors.getTempCByIndex(sensorNr);
+    readings.temp[sensorNr] = temperature;
+  }
+  return;
+}
+
 void readTankLevelSensor() {
   int loops = 10;
   int currenttanklevel = 0;
--- TemperatureSensors.ino
+++
@@ -1,9 +0,0 @@
-void readTemperatureSensors() {
-  DallasTemperature sensors(&oneWire);
-  sensors.requestTemperatures(); // Send the command to get temperatures
-  for(int sensorNr=0;sensorNr<10;sensorNr++) {
-    float temperature = sensors.getTempCByIndex(sensorNr);
-    readings.temp[sensorNr] = temperature;
-  }
-  return;
-}
